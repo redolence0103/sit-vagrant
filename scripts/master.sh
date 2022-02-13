6cc4d1145cbefeea0f9b0a2603d924c16f11337d
youngcheol kim <90162116+redolence0103@users.noreply.github.com>
2022-02-13 12:30:50 +0900

    Update master.sh

--- scripts/master.sh
+++ scripts/master.sh
@@ -79,32 +79,47 @@ kubectl -n kubernetes-dashboard get secret $(kubectl -n kubernetes-dashboard get
 kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/master/deploy/local-path-storage.yaml
 kubectl patch storageclass local-path -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
 
+# KUBECONFIG 및 VBOXSF 설정
 sudo -i -u vagrant bash << EOF
 mkdir -p /home/vagrant/.kube
 sudo cp -i /vagrant/configs/config /home/vagrant/.kube/
 sudo chmod 600 /home/vagrant/.kube/config
 sudo chown 1000:1000 /home/vagrant/.kube/config
-curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 > /home/vagrant/get_helm.sh
-chmod 700 /home/vagrant/get_helm.sh
+sudo usermod -aG vboxsf vagrant
+EOF
 
+# CODE-SERVER 설치
+sudo apt install bash-completion -y
 curl -fsSL https://code-server.dev/install.sh | sh
-sudo systemctl enable --now code-server@$USER
+sudo systemctl enable --now code-server@vagrant
 
+cat <<EOF | tee -a /home/vagrant/.config/code-server/config.yaml
+bind-addr: 10.0.0.10:8080
+auth: password
+password: sit2022
+cert: false
+EOF
+
+sudo systemctl restart code-server@vagrant
+
+# NODE LVM 확장
 sudo lvextend -l +100%FREE /dev/mapper/ubuntu--vg-ubuntu--lv
 sudo resize2fs /dev/mapper/ubuntu--vg-ubuntu--lv
 
+# HELM 설치 및 저장소 
+curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 > /home/vagrant/get_helm.sh
+chmod 700 /home/vagrant/get_helm.sh
 sudo /home/vagrant/get_helm.sh
-helm repo add stable https://charts.bitnami.com/bitnami
+helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo add influxdata https://helm.influxdata.com
 helm repo add grafana https://grafana.github.io/helm-charts
 helm install kafka bitnami/kafka --namespace kafka --create-namespace
-EOF
 
-sudo apt-get update
 
-sudo apt install openjdk-17-jdk openjdk-17-jre -y
-
+# JAVA MAVEN 설치
 sudo -i -u vagrant bash << EOF
+sudo apt-get update
+sudo apt install openjdk-17-jdk openjdk-17-jre -y
 wget https://dlcdn.apache.org/maven/maven-3/3.8.4/binaries/apache-maven-3.8.4-bin.tar.gz
 sudo mkdir -p /usr/local/apache-maven
 sudo mv apache-maven-3.8.4-bin.tar.gz /usr/local/apache-maven
@@ -112,12 +127,11 @@ cd /usr/local/apache-maven
 sudo tar -xzf apache-maven-3.8.4-bin.tar.gz
 EOF
 
+# NODE NPM 설치
 sudo apt install nodejs -y
-
 sudo apt install npm -y
 
-sudo apt install bash-completion -y
-
+# BASH 환경파일 설정
 cat <<EOF | tee -a /home/vagrant/.bashrc
 export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
 export M2_HOME=/usr/local/apache-maven/apache-maven-3.8.4
@@ -127,11 +141,15 @@ export PATH=$M2:$JAVA_HOME/bin:$PATH
 source <(kubectl completion bash)
 EOF
 
+# MAVEN 환경파일 설정
 cat <<EOF | tee -a /home/vagrant/.mavenrc
 JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
 EOF
 
+# KAFKA 확인 명령어
 cat <<EOF | tee /home/vagrant/kafka.txt
+kubectl run kafka-client --restart='Never' --image docker.io/bitnami/kafka:3.1.0-debian-10-r14 --namespace kafka --command -- sleep infinity
+kubectl exec --tty -i kafka-client --namespace kafka -- bash
 kafka-console-producer.sh \
   --broker-list kafka-0.kafka-headless.kafka.svc.cluster.local:9092 \
   --topic test
